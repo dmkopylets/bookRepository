61f0d503eef3228c53c7c028ae2e17f9826a80b5
dmKopylets <dm.kopylets@gmail.com>
2024-09-06 14:14:01 +0300

    fixed button Save on new book form

--- app/src/Form/BookType.php
+++ app/src/Form/BookType.php
@@ -7,24 +7,39 @@ use App\Entity\Category;
 use App\Entity\Tag;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class BookType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
+            ->add('title', TextType::class, [
+                'label' => 'Назва',
+                'attr' => [
+                    'placeholder' => 'Title'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Опис'
+            ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
-'choice_label' => 'title',
+                'choice_label' => 'title',
             ])
             ->add('tags', EntityType::class, [
                 'class' => Tag::class,
-'choice_label' => 'title',
-'multiple' => true,
+                'choice_label' => 'title',
+                'multiple' => true,
+            ])
+            ->add('save', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-outline-primary mt-4'
+                ]
             ])
         ;
     }
