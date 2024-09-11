788b099d11ae18894ad7b133d32210fd5db21913
dmkopylets <dm.kopylets@gmail.com>
2024-09-11 07:06:24 +0300

    form Books updating

--- app/src/Form/BookType.php
+++ app/src/Form/BookType.php
@@ -5,7 +5,6 @@ namespace App\Form;
 use App\Entity\Book;
 use App\Entity\Category;
 use App\Entity\Tag;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -13,15 +12,11 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use App\Form\TagsTransformer;
 
 class BookType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options, EntityManagerInterface $entityManager): void
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $tagRepository = $entityManager->getRepository(Tag::class);
-        $tags = $tagRepository->findAll();
         $builder
             ->add('title', TextType::class, [
                 'label' => 'Назва',
@@ -36,19 +31,16 @@ class BookType extends AbstractType
                 'class' => Category::class,
                 'choice_label' => 'title',
             ])
-            ->add('tags', ChoiceType::class, [
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
                 'label' => 'Select Tags',
-                //'choices' => Tag::class,
-                'choices' => array_map(function (Tag $tag) {
-                    return $tag->getTitle();
-                }, $tags),
+//                'choices' => Tag::class,
+//                'choices' => array_map(function (Tag $tag) {
+//                    return $tag->getTitle();
+//                }, $tags),
                 'choice_label' => 'title',
                 'multiple' => true,
                 'expanded' => true,
-            ])
-            ->add('selectedTags', TextType::class, [
-                'label' => 'Selected Tags',
-                'model_transformer' => new TagsTransformer(),
             ])
             ->add('save', SubmitType::class, [
                 'attr' => [
